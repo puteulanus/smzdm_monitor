552f217ee2e8f247c2b4a1f8aa82c8534f952f72
Puteulanus <mj@puteulanus.com>
2015-12-22 15:31:29 +0800

    修正挂载目录

--- script/config.sh
+++ script/config.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ -d "/mnt/volume/smzdm_monitor" ]; then
+	ln -s /mnt/volume/smzdm_monitor /root/conf
+fi
+
 if [ -x "/root/conf/base.sh" ]; then
 	/root/conf/base.sh
 fi
 
-if [ -d "/root/conf/main.txt" ]; then
+if [ -f "/root/conf/main.txt" ]; then
 	mv /root/conf/main.txt /root/web/list/main.txt
 fi
